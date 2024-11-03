a3845a44abcbf6c920db7c208018e6bdd366becb
abdul aziz <125464874+duls3345@users.noreply.github.com>
2024-11-03 17:45:31 +0700

    auto loading dan name space

---
+++ namespace/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'App/init.php';
+// $produk1 = new komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2= new game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+// $cetakProduk = new cetakInfoProduk();
+// $cetakProduk->tambahProduk(  $produk1 );
+// $cetakProduk->tambahProduk(  $produk2 );
+// echo $cetakProduk->cetak();
+// echo "<hr>";
+use App\service\User as serviceUser;
+use App\Produk\User as ProdukUser;
+new serviceUser();
+echo "<br>";
+new ProdukUser();
